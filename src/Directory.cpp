beb2515bfd8f780f609d4529a27e5f2bddacfb42
Igor <sala-igor@wp.pl>
2024-11-27 17:59:23 +0100

    Move function od directory.cpp created

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -12,6 +12,16 @@ void Directory::add(Object & object)
 
 void Directory::move(Object & object)
 {
+    std::stringstream ss(object._name);
+    std::string token;
+    std::string nameOfFiles;
+    while (std::getline(ss, token, "/"))
+        ;
 
+    fs::path source(object._name);
+    fs::path destination(this->_name + '/' + token);
+
+    fs::rename(source, destination);
+    object._name = this->_name + "/" + token;
 }
 
