955357c19eda52fd4f9a53cfeff0b4a73e288b74
Igor <sala-igor@wp.pl>
2024-11-27 17:28:31 +0100

    Init recreated

--- src/Directory.cpp
+++ src/Directory.cpp
@@ -3,11 +3,25 @@
 void Directory::init(std::string path)
 {
     this->Object::init(path);
+    
+    if(std::filesystem::create_directory(_name))
+    {
+        std::cout << "Directory created succesfully and tracked" << std::endl;
+    }else{
+        if(std::filesystem::exists(_name))
+        {
+            std::cout << "Directory already exists now you track it" << std::endl;
+        }else
+        {
+            std::cerr << "Something went wrong" << std::endl;
+            throw 1;
+        }
+    }
 }
 
 void Directory::add(Object & object)
 {
-
+    
 }
 
 void Directory::move(Object & object)
