f8b9f03e0e175ef6a8def628fc843c8e85d92e51
Igor <sala-igor@wp.pl>
2024-11-27 17:10:11 +0100

    First version of append

--- src/File.cpp
+++ src/File.cpp
@@ -3,27 +3,24 @@
 void File::init(std::string path)
 {
     this->Object::init(path);
-
-    std::ofstream file(_name, std::ios::app);
-    if (!file.is_open())
-    {
-        std::cerr << "Nie udało się otworzyć pliku: " << path << std::endl;
-        throw 1;
-    }else{
-        std::cout << "Sledzisz plik: " << _name << std::endl;
-        file.close();
-    }
 }
 
 void File::appendLineTxt(std::string text)
 {
     std::ofstream file(_name, std::ios::app);
     file << text << "\n";
+    file.close();
 }
 
 void File::append(const File & file)
 {
+    std::ifstream infile(file._name);
+    std::string temp;
 
+    while (std::getline(infile, temp))
+    {
+        this->appendLineTxt(temp);
+    }
 }
 
 void File::deleteFile()
