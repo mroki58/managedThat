4596c1c134ace28ae9bc996cddb2b78b3f598e37
Igor <sala-igor@wp.pl>
2024-11-27 12:55:55 +0100

    New function definition

--- src/File.cpp
+++ src/File.cpp
@@ -4,10 +4,11 @@ void File::init(std::string path)
 {
     this->Object::init(path);
 
-    std::ofstream file(_name, std::ios::out);
+    std::ofstream file(_name, std::ios::app);
     if (!file.is_open())
     {
         std::cerr << "Nie udało się otworzyć pliku: " << path << std::endl;
+        throw 1;
     }else{
         std::cout << "Sledzisz plik: " << _name << std::endl;
         file.close();
@@ -16,7 +17,8 @@ void File::init(std::string path)
 
 void File::appendLineTxt(std::string text)
 {
-
+    std::ofstream file(_name, std::ios::app);
+    file << text << "\n";
 }
 
 void File::append(const File & file)
