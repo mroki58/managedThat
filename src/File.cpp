1cabd68f0c77c42a6d4ecf5a3ccd9890e5350bb0
Igor <sala-igor@wp.pl>
2024-11-27 18:08:38 +0100

    Implemented function

--- src/File.cpp
+++ src/File.cpp
@@ -1,4 +1,6 @@
 #include "File.h"
+#include <fstream>
+#include <iostream>
 
 void File::init(std::string name)
 {
@@ -12,10 +14,19 @@ void File::appendLineTxt(std::string text)
 
 void File::append(const File & file)
 {
-
+    std::cout << "Append file to file\n";
+    std::ifstream in(file._name);
+    std::string token;
+    while(std::getline(in, token))
+    {
+        std::cout << token << " line copied!\n";
+        std::fstream out(this->_name,std::ios::app);
+        out << "\n" + token;
+    }
+    in.close();
 }
 
 void File::deleteFile()
 {
-
+    
 }
